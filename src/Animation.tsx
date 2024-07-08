4aad74ab60efa51638576859bad1d4c0d4e12458
Ruby Hu <60735962+HuWenShin@users.noreply.github.com>
2024-07-08 09:59:00 -0700

    Fix import issues with the pixi library
    Deleted the pixi library for now so I don't get errors in my repo

--- src/Animation.tsx
+++ src/Animation.tsx
@@ -1,2 +1,11 @@
-import {Stage, Container, Sprite, Text} from '@pixi/react';
+// import {Stage, Container, Sprite, Text} from '@pixi/react';
+// import * as PIXI from 'pixi.js';
+// import { Emitter, ParticleUtils } from '@pixi/particle-emitter';
+// import React, { useEffect, useRef } from 'react';
 
+// function Whale() {
+    
+//     return (
+//         <canvas></canvas>
+//     );
+// }
